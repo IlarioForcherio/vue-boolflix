e1cb22e95607e6a6da21541c6f7e454a37108fb5
ilarioforcherio <ilarioforcherio@gmail.com>
2022-11-29 11:09:59 +0100

    create document

---
+++ src/components/MainComp.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="">
+  ciao
+</div>
+</template>
+
+<script>
+export default {
+  name: 'MainComp'
+  // props: {
+    
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import '/Users/ilarioforcherio/Desktop/BOOLEAN/esercizi/Vue-Cli/vue-boolflix/node_modules/bootstrap/dist/css/bootstrap.rtl.min.css'
+import '/Users/ilarioforcherio/Desktop/BOOLEAN/esercizi/Vue-Cli/vue-boolflix/node_modules/bootstrap/dist/js/bootstrap.min.js'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
